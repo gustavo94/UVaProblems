bafdba18596948d99d82c0a3251cacaa60f1e263
Gustavo Preciado <tavo.lpj.94@gmail.com>
2017-03-05 19:14:51 -0500

    Fixed the test cases in the template

--- UVa-python-template.py
+++ UVa-python-template.py
@@ -1,10 +1,13 @@
 from sys import stdin
+# exapmle input:
+ex_in = "6 3 1 10\n10 2 1 50\n50 5 3 14\n50 6 4 1\n50 6 3 1\n1 1 1 1\n0 0 0 0\n"
 
 def main():
 	lines = stdin.read().splitlines() #Use ctrl+d to finish read process
+	#lines = ex_in.splitlines() # Testing
 	for curr_Line in lines:
 		print(curr_Line)
-		
-	return 
+
+	return
 
 main()
